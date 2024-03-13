cd0cb200b5bcb3fb0bd8a74c0ae19744e9d9fa43
Kelvin Pichinini <kelvinpichinini@gmail.com>
2024-03-13 19:13:47 -0300

    feat: first commit - initial configuration

---
+++ src/main/java/com/kelbank/KelbankApplication.java
@@ -0,0 +1,13 @@
+package com.kelbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelbankApplication.class, args);
+	}
+
+}
