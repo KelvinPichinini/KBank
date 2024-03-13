81ad3f0ca9398caef5814c359241982c5358b36e
Kelvin Pichinini <kelvinpichinini@gmail.com>
2024-03-13 19:35:38 -0300

    feat: creating User service

---
+++ src/main/java/com/kelbank/services/UserService.java
@@ -0,0 +1,29 @@
+package com.kelbank.services;
+
+import com.kelbank.domain.user.User;
+import com.kelbank.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void createUser(User user){
+        this.repository.save(user);
+    }
+}
