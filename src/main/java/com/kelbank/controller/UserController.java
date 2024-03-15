bf09ebce1be51a2d6b6748d3b9a0617c56431b3a
Kelvin Pichinini <kelvinpichinini@gmail.com>
2024-03-15 20:38:11 -0300

    feat: creating User Controller

---
+++ src/main/java/com/kelbank/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.kelbank.controller;
+
+import com.kelbank.domain.user.User;
+import com.kelbank.dtos.UserDTO;
+import com.kelbank.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+@RestController()
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user){
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> allUsers = userService.getAllUsers();
+        return new ResponseEntity<>(allUsers,HttpStatus.OK);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> changeEmail(@RequestBody Map<String, String> body, @PathVariable Long id) throws Exception {
+            String email = body.get("email");
+            User user = userService.changeEmail(email,id);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("/users/cash-in/{id}")
+    public ResponseEntity<User> addFunds(@RequestBody Map<String, String> body, @PathVariable Long id) throws Exception {
+        BigDecimal sumAmount = new BigDecimal(body.get("amount"));
+        User user = userService.addFunds(id, sumAmount);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteAccount(@PathVariable Long id) throws Exception {
+        userService.deleteUser(id);
+        return new ResponseEntity<>("Usuário deletado com sucesso", HttpStatus.OK);
+
+    }
+}
